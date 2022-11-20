0b010d18b85a2bfa8acd3e20ff9c818136cfc8bc
jugnpengchen <jungpeng.chen@gmail.com>
2022-11-21 02:15:27 +0800

    [UPDATE]update test

--- src/tests/test_concurrency.py
+++ src/tests/test_concurrency.py
@@ -8,9 +8,8 @@ class TestMultithread(LiveServerTestCase):
         return server.app
     def setUp(self):
         server.init_task_list()
-    def test_create_100_tasks_concurrency(self):
+    def create_n_concurrent_tasks(self, thread_counts):
         input_task_name = 'task_name'
-        thread_counts = 100
         url = self.get_server_url()
         client = requests
 
@@ -18,16 +17,27 @@ class TestMultithread(LiveServerTestCase):
             resp = client.post(f'{url}/task', json={'name': f'{input_task_name}-{task_seq}'})
             return resp.status_code
 
-        expect_status_code_dict = {201: thread_counts}
-        resp_status_code_dict  = {}
         with Pool(max_workers=thread_counts) as pool:
             result = pool.map(return_status_code, [i for i in range(1,thread_counts+1)])
+        return result
+
+    def test_create_100_concurrent_tasks(self):
+        thread_counts = 100
+        result = self.create_n_concurrent_tasks(thread_counts)
+        expect_status_code_dict = {201: thread_counts}
+
+        resp_status_code_dict  = {}
         for resp_status_code in result:
             if resp_status_code in resp_status_code_dict:
                 resp_status_code_dict[resp_status_code] += 1
             else:
                 resp_status_code_dict[resp_status_code] = 1
         self.assertEqual(resp_status_code_dict, expect_status_code_dict)
+    def test_list_tasks_after_create_100_concurrent_tasks(self):
+        thread_counts = 100
+        url = self.get_server_url()
+        client = requests
+        self.create_n_concurrent_tasks(thread_counts)
 
         resp = client.get(f'{url}/tasks')
         resp_task_list = resp.json()['result']
